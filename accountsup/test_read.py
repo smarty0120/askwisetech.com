15fb330afc611bb51e7581fcf5895c1cbbe439be
smarty0120 <amankumar1925009@gmail.com>
2026-06-15 14:28:54 +0530

    Delete accountsup directory

--- accountsup/test_read.py
+++
@@ -1,7 +0,0 @@
-import os
-try:
-    path = r"C:\Users\asfin\.gemini\antigravity-ide\brain\c2c6cc0f-b257-40c9-99e0-f6d5a1420b73\media__1781503821200.png"
-    with open(path, "rb") as f:
-        print("Success:", f.read(10))
-except Exception as e:
-    print("Error:", e)
